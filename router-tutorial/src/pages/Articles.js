cf85297bf102f0e52c88471f22b09f9df8ef731f
DongwookJeong <nolhoon211@gmail.com>
2023-04-26 04:27:38 +0900

    NavLink 리팩터링 방향

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,28 +1,31 @@
-import {NavLink, Outlet} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Articles = () => {
+    return (
+        <div>
+            <ul>
+                <ArticleItem id = {1}/>
+                <ArticleItem id = {2}/>
+                <ArticleItem id = {3}/>
+            </ul>
+        </div>
+    )
+}
+
+const ArticleItem = ({id})=>{
     const activeStyle = {
-        color: 'green',
+        color: 'blue',
         fontSize: 21,
     }
     return (
-        <div>
-            <Outlet/>
-        <ul>
-            <li>
-                <NavLink to="/articles/1" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 1</NavLink>
-            </li>
-        
-            <li>
-                <NavLink to="/articles/2" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 2</NavLink>
-
-            </li>
-            <li>
-               <NavLink to="/articles/3" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 3</NavLink>
-
-            </li>
-        </ul>
-        </div>
+        <li>
+            <NavLink
+                to = {`/articles/${id}`}
+                style = {({isActive}) => (isActive ? activeStyle : undefined)}
+            >
+                게시글 {id}
+            </NavLink>
+        </li>
     )
 }
 
